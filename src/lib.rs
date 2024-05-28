a366f830a3517c60939e5e929b8180a9f3e99c58
Rajput, Rajat <Rajat.Rajput@aristocrat.com>
2024-05-28 19:54:21 +0530

    added future 'map' feature. at the poing I'm quite happy with myself.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+use core::panic;
 use std::{
     fmt::Debug,
     mem,
     sync::RwLock,
     thread::{self, JoinHandle},
 };
+use thiserror::Error as ThisError;
 use tracing::{error, trace};
 
 #[derive(Debug)]
@@ -12,7 +14,10 @@ pub enum Future<T> {
     Pending(oneshot::Receiver<T>),
 }
 
-impl<T: Debug> Future<T> {
+impl<T> Future<T>
+where
+    T: Debug + Send + 'static,
+{
     pub fn poll(&mut self) {
         if let Future::Pending(receiver) = self {
             match receiver.try_recv() {
@@ -27,6 +32,29 @@ impl<T: Debug> Future<T> {
             }
         }
     }
+
+    pub fn map<U, F>(self, f: F) -> Future<U>
+    where
+        U: Send + 'static,
+        F: FnOnce(T) -> U + Send + 'static,
+    {
+        match self {
+            Future::Ready(t) => ThreadPool::run_async(move || f(t)),
+            Future::Pending(fut) => ThreadPool::run_async(move || match fut.recv() {
+                Ok(t) => f(t),
+                Err(_) => {
+                    error!("execution of mapped future failed");
+                    panic!("mapped future problem")
+                }
+            }),
+        }
+    }
+}
+
+#[derive(Debug, ThisError)]
+pub enum Error {
+    #[error("the future being mapped was ready already")]
+    FutureWasReady,
 }
 
 pub struct ThreadPool {
@@ -42,7 +70,7 @@ impl ThreadPool {
     {
         let (sender, receiver) = oneshot::channel();
         let future = Future::Pending(receiver);
-        THREAD_POOL.read().unwrap().as_ref().map(|thread_pool| {
+        if let Some(thread_pool) = THREAD_POOL.read().unwrap().as_ref() {
             thread_pool
                 .distributor
                 .send(Box::new(|| {
@@ -50,7 +78,7 @@ impl ThreadPool {
                     sender.send(f()).unwrap();
                 }))
                 .unwrap()
-        });
+        }
         future
     }
 
@@ -94,10 +122,7 @@ lazy_static::lazy_static! {
 
 impl<T: PartialEq> PartialEq for Future<T> {
     fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (Future::Ready(a), Future::Ready(b)) if a == b => true,
-            _ => false,
-        }
+        matches!((self, other), (Future::Ready(a), Future::Ready(b)) if a == b)
     }
 }
 
@@ -116,22 +141,60 @@ mod tests {
         };
         trace!("pushing a function to run asynchronously");
         let mut value = ThreadPool::run_async(magic_number);
-        trace!("first check");
         if let Future::Pending(_) = value {
-            assert!(true);
+            trace!("before sleep and poll the value should be pending and it is");
         } else {
-            assert!(false);
+            unreachable!("before sleep and poll the future must be pending");
         }
         thread::sleep(Duration::from_millis(101));
-        trace!("second check");
         if let Future::Pending(_) = value {
-            assert!(true);
+            trace!("after sleep but before polling, the future should be pending. and it is");
         } else {
-            assert!(false);
+            unreachable!("after sleep but before polling, the future should be pending");
         }
         value.poll();
         trace!("third check");
         assert_eq!(value, Future::Ready(42));
+        Ok(())
+    }
+
+    #[test]
+    fn nested_future() -> AnyResult<()> {
+        let _ = tracing_subscriber::fmt::try_init();
+        let magic_num_fut = ThreadPool::run_async(|| {
+            thread::sleep(Duration::from_millis(100));
+            42
+        });
+        if let Future::Pending(_) = magic_num_fut {
+            trace!("before polling the first future should be a pending");
+        } else {
+            unreachable!("before polling shouldn't be 'ready'");
+        }
+        let mut double_num_fut = magic_num_fut.map(|n| {
+            thread::sleep(Duration::from_millis(100));
+            n * 2
+        });
+        if let Future::Pending(_) = double_num_fut {
+            trace!("before polling the nested future should be pending");
+        } else {
+            unreachable!("before polling the nested future shouldn't be 'ready'");
+        }
+        double_num_fut.poll();
+        if let Future::Pending(_) = double_num_fut {
+            trace!( "first polling is done, but both the future are going to resolve in 100 msec each, so pending it is");
+        } else {
+            unreachable!("after first polling, before 200 msec delay, no ready");
+        }
+        thread::sleep(Duration::from_millis(105));
+        double_num_fut.poll();
+        if let Future::Pending(_) = double_num_fut {
+            trace!( "second polling is done, but only the future is going to resolve in 100 msec, so pending it is");
+        } else {
+            unreachable!("after second polling, before 100 msec delay, no ready");
+        }
+        thread::sleep(Duration::from_millis(105));
+        double_num_fut.poll();
+        assert_eq!(double_num_fut, Future::Ready(84));
         Ok(())
     }
 }
