3517a901eab16e6d87950eaa3a5c03e2e8b2cca5
Rajput, Rajat <Rajat.Rajput@aristocrat.com>
2024-05-28 17:24:17 +0530

    basic test is passed. in next commit traces will also have thread-ids

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use std::{
 };
 use tracing::{error, trace};
 
+#[derive(Debug)]
 pub enum Future<T> {
     Ready(T),
     Pending(oneshot::Receiver<T>),
@@ -41,39 +42,39 @@ impl ThreadPool {
     {
         let (sender, receiver) = oneshot::channel();
         let future = Future::Pending(receiver);
-        THREAD_POOL
-            .read()
-            .unwrap()
-            .as_ref()
-            .map_or_else(Self::create, |thread_pool| {
-                thread_pool
-                    .distributor
-                    .send(Box::new(|| {
-                        sender.send(f()).unwrap();
-                    }))
-                    .unwrap()
-            });
+        THREAD_POOL.read().unwrap().as_ref().map(|thread_pool| {
+            thread_pool
+                .distributor
+                .send(Box::new(|| {
+                    trace!("this is a job cb being scheduled to run");
+                    sender.send(f()).unwrap();
+                }))
+                .unwrap()
+        });
         future
     }
 
-    fn create() {
+    fn create() -> Self {
         let (distributor, receiver) = crossbeam::channel::unbounded();
         let num_threads = thread::available_parallelism().unwrap().get();
         let num_of_threads_to_use = if num_threads <= 2 { 1 } else { num_threads - 2 };
-        let _ = THREAD_POOL.write().unwrap().insert(ThreadPool {
+        ThreadPool {
             threads: (0..num_of_threads_to_use)
                 .map(|_| {
                     let receiver: crossbeam::channel::Receiver<Box<dyn FnOnce() + Send>> =
                         receiver.clone();
                     thread::spawn(move || {
+                        trace!("thread is spawned!");
                         while let Ok(f) = receiver.recv() {
+                            trace!("thread has received a job!");
                             f();
+                            trace!("thread has completed running the job");
                         }
                     })
                 })
                 .collect(),
             distributor,
-        });
+        }
     }
 }
 
@@ -87,4 +88,50 @@ impl Drop for ThreadPool {
     }
 }
 
-static THREAD_POOL: RwLock<Option<ThreadPool>> = RwLock::new(None);
+lazy_static::lazy_static! {
+    static ref THREAD_POOL: RwLock<Option<ThreadPool>> = RwLock::new(Some(ThreadPool::create()));
+}
+
+impl<T: PartialEq> PartialEq for Future<T> {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Future::Ready(a), Future::Ready(b)) if a == b => true,
+            _ => false,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::Result as AnyResult;
+    use std::time::Duration;
+
+    #[test]
+    fn simple_test() -> AnyResult<()> {
+        let _ = tracing_subscriber::fmt::try_init();
+        let magic_number = || {
+            thread::sleep(Duration::from_millis(100));
+            42
+        };
+        trace!("pushing a function to run asynchronously");
+        let mut value = ThreadPool::run_async(magic_number);
+        trace!("first check");
+        if let Future::Pending(_) = value {
+            assert!(true);
+        } else {
+            assert!(false);
+        }
+        thread::sleep(Duration::from_millis(100));
+        trace!("second check");
+        if let Future::Pending(_) = value {
+            assert!(true);
+        } else {
+            assert!(false);
+        }
+        value.poll();
+        trace!("third check");
+        assert_eq!(value, Future::Ready(42));
+        Ok(())
+    }
+}
