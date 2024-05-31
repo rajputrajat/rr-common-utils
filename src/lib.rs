b4d0ec40dd8d167e62315a834643c30e9895fd29
Rajput, Rajat <Rajat.Rajput@aristocrat.com>
2024-05-31 13:32:45 +0530

    added support for selecting only one Future and ignore all others

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use core::panic;
 use std::{
+    borrow::Cow,
     fmt::Debug,
     mem,
-    sync::RwLock,
+    sync::{mpsc, OnceLock},
     thread::{self, JoinHandle},
 };
 use thiserror::Error as ThisError;
@@ -39,8 +40,8 @@ where
         F: FnOnce(T) -> U + Send + 'static,
     {
         match self {
-            Future::Ready(t) => ThreadPool::run_async(move || f(t)),
-            Future::Pending(fut) => ThreadPool::run_async(move || match fut.recv() {
+            Future::Ready(t) => ThreadPool::global().run_async(move || f(t)),
+            Future::Pending(fut) => ThreadPool::global().run_async(move || match fut.recv() {
                 Ok(t) => f(t),
                 Err(_) => {
                     error!("execution of mapped future failed");
@@ -57,8 +58,8 @@ where
         E: Debug + Send + 'static,
     {
         match self {
-            Future::Ready(t) => ThreadPool::run_async(move || f(t)),
-            Future::Pending(fut) => ThreadPool::run_async(move || match fut.recv() {
+            Future::Ready(t) => ThreadPool::global().run_async(move || f(t)),
+            Future::Pending(fut) => ThreadPool::global().run_async(move || match fut.recv() {
                 Ok(t) => f(t),
                 Err(_) => {
                     error!("execution of mapped future failed");
@@ -76,6 +77,38 @@ where
     }
 }
 
+pub trait SelectFuture<T>: Iterator<Item = Future<T>> + Sized
+where
+    T: Send + 'static + Debug,
+{
+    #[tracing::instrument(skip_all)]
+    fn select(self) -> Future<T> {
+        let (sender, receiver) = mpsc::channel();
+        for fut in self {
+            let sender = sender.clone();
+            ThreadPool::global().run_async(move || {
+                match fut {
+                    Future::Ready(t) => {
+                        trace!("data '{t:?}' was ready so send, without waiting");
+                        let _ = sender.send(t);
+                    }
+                    Future::Pending(one_receiver) => {
+                        if let Ok(t) = one_receiver.recv() {
+                            trace!("data '{t:?}' received from future now send");
+                            let _ = sender.send(t);
+                        }
+                    }
+                };
+            });
+        }
+        let (oneshot_sender, oneshot_receiver) = oneshot::channel();
+        ThreadPool::global().run_async(move || {
+            oneshot_sender.send(receiver.recv().unwrap()).unwrap();
+        });
+        Future::Pending(oneshot_receiver)
+    }
+}
+
 #[derive(Debug, ThisError)]
 pub enum Error {
     #[error("the future being mapped was ready already")]
@@ -83,64 +116,60 @@ pub enum Error {
 }
 
 pub struct ThreadPool {
+    name: Cow<'static, str>,
     threads: Vec<JoinHandle<()>>,
     distributor: crossbeam::channel::Sender<Box<dyn FnOnce() + Send + 'static>>,
 }
 
 impl ThreadPool {
-    pub fn run_async<F, T>(f: F) -> Future<T>
+    #[tracing::instrument(skip(self, f), fields(self.name = %self.name))]
+    pub fn run_async<F, T>(&self, f: F) -> Future<T>
     where
         F: FnOnce() -> T + Send + 'static,
         T: Debug + Send + 'static,
     {
         let (sender, receiver) = oneshot::channel();
-        let future = Future::Pending(receiver);
-        if let Some(thread_pool) = THREAD_POOL.read().unwrap().as_ref() {
-            thread_pool
-                .distributor
-                .send(Box::new(|| {
-                    if sender.is_closed() {
-                        warn!("receiver of {sender:?} is closed, so skipping run of attached callback");
-                    } else {
-                        trace!("this is a job cb being scheduled to run");
-                        sender.send(f()).unwrap();
-                    }
-                }))
-                .unwrap()
-        }
-        future
+        self.distributor
+            .send(Box::new(|| {
+                if sender.is_closed() {
+                    warn!("receiver of {sender:?} is closed, so skipping run of attached callback");
+                } else {
+                    trace!("this is a job cb being scheduled to run");
+                    sender.send(f()).unwrap();
+                }
+            }))
+            .unwrap();
+
+        Future::Pending(receiver)
     }
 
-    pub fn try_run_async<F, T, E>(f: F) -> Future<Result<T, E>>
+    pub fn try_run_async<F, T, E>(&self, f: F) -> Future<Result<T, E>>
     where
         F: FnOnce() -> Result<T, E> + Send + 'static,
         T: Debug + Send + 'static,
         E: Debug + Send + 'static,
     {
         let (sender, receiver) = oneshot::channel();
-        let future = Future::Pending(receiver);
-        if let Some(thread_pool) = THREAD_POOL.read().unwrap().as_ref() {
-            thread_pool
-                .distributor
-                .send(Box::new(|| {
-                    if sender.is_closed() {
-                        warn!("receiver of {sender:?} is closed, so skipping run of attached callback");
-                    } else {
-                        trace!("this is a job cb being scheduled to run");
-                        sender.send(f()).unwrap();
-                    }
-                }))
-                .unwrap()
-        }
-        future
+        self.distributor
+            .send(Box::new(|| {
+                if sender.is_closed() {
+                    warn!("receiver of {sender:?} is closed, so skipping run of attached callback");
+                } else {
+                    trace!("this is a job cb being scheduled to run");
+                    sender.send(f()).unwrap();
+                }
+            }))
+            .unwrap();
+
+        Future::Pending(receiver)
     }
 
-    fn create() -> Self {
+    #[tracing::instrument]
+    pub fn create<N: Into<Cow<'static, str>> + Debug>(name: N, num_of_threads: usize) -> Self {
         let (distributor, receiver) = crossbeam::channel::unbounded();
-        let num_threads = thread::available_parallelism().unwrap().get();
-        let num_of_threads_to_use = if num_threads <= 2 { 1 } else { num_threads - 2 };
         ThreadPool {
-            threads: (0..num_of_threads_to_use)
+            name: name.into(),
+            threads: (0..num_of_threads)
                 .map(|thread_id| {
                     let receiver: crossbeam::channel::Receiver<Box<dyn FnOnce() + Send>> =
                         receiver.clone();
@@ -157,7 +186,13 @@ impl ThreadPool {
             distributor,
         }
     }
+
+    pub fn global() -> &'static Self {
+        GLOBAL_THREAD_POOL.get_or_init(|| ThreadPool::create("Global", 128))
+    }
 }
+
+static GLOBAL_THREAD_POOL: OnceLock<ThreadPool> = OnceLock::new();
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
@@ -167,10 +202,6 @@ impl Drop for ThreadPool {
             th.join().unwrap();
         }
     }
-}
-
-lazy_static::lazy_static! {
-    static ref THREAD_POOL: RwLock<Option<ThreadPool>> = RwLock::new(Some(ThreadPool::create()));
 }
 
 impl<T: PartialEq> PartialEq for Future<T> {
@@ -193,7 +224,7 @@ mod tests {
             42
         };
         trace!("pushing a function to run asynchronously");
-        let mut value = ThreadPool::run_async(magic_number);
+        let mut value = ThreadPool::global().run_async(magic_number);
         if let Future::Pending(_) = value {
             trace!("before sleep and poll the value should be pending and it is");
         } else {
@@ -214,7 +245,7 @@ mod tests {
     #[test]
     fn nested_future() -> AnyResult<()> {
         let _ = tracing_subscriber::fmt::try_init();
-        let magic_num_fut = ThreadPool::run_async(|| {
+        let magic_num_fut = ThreadPool::global().run_async(|| {
             thread::sleep(Duration::from_millis(100));
             42
         });
